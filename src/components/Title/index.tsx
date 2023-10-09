444b1c06c044b0d8daf0aea2438464574dd67f47
marcelojob <marcelojob1995@gmail.com>
2023-10-09 17:49:24 -0300

    fix: adjust lint

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -9,6 +9,6 @@ const Title: React.FC<TitleProps> = ({ children }) =>  {
   return(
     <h2 className="text-green-600 mb-5 font-semibold text-xl lg:text-2xl">{children}</h2>
   )
-}
+};
 
 export default Title;
