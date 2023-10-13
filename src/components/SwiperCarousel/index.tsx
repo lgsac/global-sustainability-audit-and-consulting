55633aa397346de7ef366a8345f31dac32d6a79c
marcelojob <marcelojob1995@gmail.com>
2023-10-13 11:36:53 -0300

    feat: created carousel component

---
+++ src/components/SwiperCarousel/index.tsx
@@ -0,0 +1,52 @@
+import React, { ReactNode } from 'react';
+import { Swiper } from 'swiper/react';
+import { Navigation, A11y} from 'swiper/modules';
+import "swiper/css";
+import 'swiper/css/navigation';
+import Image from 'next/image';
+
+type SwiperCarouselProps = {
+  children: ReactNode ;
+}
+
+const SwiperCarousel: React.FC<SwiperCarouselProps> = ({ children }) =>  {
+
+  return(
+    <>
+      <div className="swiper-button swiper-btn-next">
+        <Image
+          src="/next-button.svg"
+          alt=""
+          width={30}
+          height={0}
+          priority
+        />
+      </div>
+      <div className="swiper-button swiper-btn-prev">
+        <Image
+          src="/prev-button.svg"
+          alt=""
+          width={30}
+          height={0}
+          priority
+        />
+      </div>
+      <Swiper
+        modules={[Navigation, A11y]}
+        navigation={{
+          nextEl: ".swiper-btn-next",
+          prevEl: ".swiper-btn-prev",
+          disabledClass: ".swiper-btn-disabled"
+        }}
+        autoHeight={true}
+        spaceBetween={15}
+        slidesPerView={3}
+
+      >
+        {children}
+      </Swiper>
+    </>
+  )
+};
+
+export default SwiperCarousel;
